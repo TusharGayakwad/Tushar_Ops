fe378981c4d2c849f4f588a51acbd6de14e0fd75
Tushar Gayakwad <109648133+TusharGayakwad@users.noreply.github.com>
2025-05-06 23:39:52 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host = 'smtp.gmail.com';  
         $mail->SMTPAuth = true;  
         $mail->Username = 'tushargayakwad29@gmail.com'; // Your Gmail address
-        $mail->Password = 'qxjr kxiu rnic zosg'; // Your Gmail App Password (make sure it's correct)
+        $mail->Password = 'app password'; // Your Gmail App Password (make sure it's correct)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
